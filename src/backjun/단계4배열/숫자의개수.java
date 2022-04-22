a59c0345001d099e718724f82362da4e51d66f3a
yeonjin <y_jin99@naver.com>
2022-04-22 23:53:51 +0900

    단계4 : 배열

---
+++ src/backjun/단계4배열/숫자의개수.java
@@ -0,0 +1,26 @@
+package backjun.단계4배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 숫자의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] value = String.valueOf(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine())).toCharArray();
+        int[] count = new int[10];
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : value){
+            for (int i =0;i<10;i++){
+                if((c-'0')==i) count[i]+=1;
+            }
+
+        }
+
+        for(int num : count){
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
