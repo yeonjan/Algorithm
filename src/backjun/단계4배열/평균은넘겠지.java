41ea06af5dc2997d3bbdc3d193f57afa1ac4ff49
Yeonjin Jeong <y_jin99@naver.com>
2022-04-24 21:37:46 +0900

    단계 4 : 배열

---
+++ src/backjun/단계4배열/평균은넘겠지.java
@@ -0,0 +1,35 @@
+package backjun.단계4배열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 평균은넘겠지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int testCase = 0; testCase < c; ++testCase) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            int count = 0;
+
+            int[] score = new int[n];
+            for (int i = 0; i < n; ++i) {
+                score[i] = Integer.parseInt(st.nextToken());
+                sum += score[i];
+            }
+            for (int s : score) {
+                if (s > sum / n) count++;
+            }
+            sb.append(String.format("%.3f%%\n", (double) count * 100 / n));
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
