c42b46ea0d3af33328795e2c3d2e0803b570a9df
Yeonjin Jeong <y_jin99@naver.com>
2022-04-27 18:42:59 +0900

    단계 6 : 배열

--- src/backjun/단계6문자열/단어공부.java
+++ src/backjun/단계6문자열/단어공부.java
@@ -3,7 +3,6 @@ package backjun.단계6문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class 단어공부 {
     static int ALPHABET = 26;
@@ -11,8 +10,9 @@ public class 단어공부 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] word = br.readLine().toUpperCase().toCharArray();
         int[] count = new int[ALPHABET];
-        int max = 0;
+        int max = -1;
         int idx = -1;
+        boolean is_duplicate = false;
 
         //알파벳 개수 세기
         for (char a : word) {
@@ -21,17 +21,18 @@ public class 단어공부 {
 
         //가장 많이 나온 알파벳과 위치 계산
         for (int i = 0; i < ALPHABET; i++) {
-            if (max <= count[i]) {
+            if (max < count[i]) {
+                is_duplicate=false;
                 max = count[i];
                 idx = i;
+            } else if (max == count[i]) {
+                is_duplicate=true;
             }
         }
 
-        String scount = Arrays.toString(count).replaceAll("[^0-9]","");
-        int max_idx=scount.indexOf(String.valueOf(max));
-        if(idx==max_idx){
-            System.out.println((char) (idx+'A'));
-        }else System.out.println("?");
+        if (!is_duplicate) {
+            System.out.println((char) (idx + 'A'));
+        } else System.out.println("?");
 
     }
 }
