d66ab970fbeb9d9126b44805f2af7c5a70ff00cf
Yeonjin Jeong <y_jin99@naver.com>
2022-04-30 15:05:56 +0900

    단계 7 : 기본수학1

---
+++ src/backjun/단계7기본수학1/부녀회장1.java
@@ -0,0 +1,32 @@
+package backjun.단계7기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 부녀회장1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; ++i) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            System.out.println(howMany(k, n));
+        }
+
+    }
+
+    public static int howMany(int k, int n) {
+        int people = 0;
+        if (k == 0) {
+            return n;
+        } else {
+            for (int i = 1; i <= n; i++) {
+            people += howMany(k-1,i);
+            }
+            return people;
+        }
+
+    }
+}
---
+++ src/backjun/단계7기본수학1/부녀회장2.java
@@ -0,0 +1,7 @@
+package backjun.단계7기본수학1;
+
+public class 부녀회장2 {
+    public static void main(String[] args) {
+
+    }
+}
