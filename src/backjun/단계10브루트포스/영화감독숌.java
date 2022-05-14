c9b0d95fe2977d832b2d11a4543dfa124e288665
Yeonjin Jeong <y_jin99@naver.com>
2022-05-14 22:57:46 +0900

    단계 10 : 브루트포스

---
+++ src/backjun/단계10브루트포스/영화감독숌.java
@@ -0,0 +1,20 @@
+package backjun.단계10브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 영화감독숌 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 665;
+
+        while (n > 0) {
+            result++;
+            if (String.valueOf(result).contains("666")) n--;
+        }
+
+        System.out.println(result);
+    }
+}
