59363ef781350c80fb80ff726cad32d1a2ef3113
Yeonjin Jeong <y_jin99@naver.com>
2022-07-21 23:22:34 +0900

    [다이나믹 프로그래밍] 조건이 있는 dp

---
+++ src/backjun/알고리즘기초/다이나믹프로그래밍1/가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,33 @@
+package backjun.알고리즘기초.다이나믹프로그래밍1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장_긴_증가하는_부분_수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        int[] dp = new int[n];
+        int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+            }
+            answer = Math.max(dp[i], answer);
+
+        }
+        System.out.println(answer);
+
+    }
+}
