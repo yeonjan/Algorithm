79da1e3ee64a051f7473015495622ade6dfa8fbb
Yeonjin Jeong <y_jin99@naver.com>
2022-07-25 23:21:09 +0900

    [그리디]

---
+++ src/study/A_B_16953.java
@@ -0,0 +1,36 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A_B_16953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 1;
+
+        while (b!=a) {
+            if(b<a) {
+                cnt=-1;
+                break;
+            }
+            if (b % 2 == 0) {
+                b /= 2;
+                cnt++;
+            } else if (b % 10 == 1) {
+                b /= 10;
+                cnt++;
+            } else {
+                cnt = -1;
+                break;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
