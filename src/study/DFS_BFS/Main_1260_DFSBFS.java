308174a066e15b8ffdc8da442410dff47975679f
Yeonjin Jeong <y_jin99@naver.com>
2022-08-16 15:28:38 +0900

    dfs,bfs

---
+++ src/study/DFS_BFS/Main_1260_DFSBFS.java
@@ -0,0 +1,79 @@
+package study.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_1260_DFSBFS {
+    static int N, M, V;
+    static boolean[] visit;
+    static int[] selected;
+    static List<Integer>[] edge;
+    static Queue<Integer> queue = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        visit = new boolean[N + 1];
+        selected = new int[N];
+        edge = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            edge[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            edge[a].add(b);
+            edge[b].add(a);
+        }
+        for (List<Integer> e : edge) {
+            Collections.sort(e);
+        }
+
+        dfs(V, visit, 0);
+        sb.append("\n");
+
+        visit = new boolean[N + 1];
+        queue.add(V);
+        int cnt = 0;
+        visit[V] = true;
+        while (!queue.isEmpty()) {
+            bfs(queue.poll(), visit, cnt++);
+        }
+        sb.append("\n");
+
+        System.out.println(sb.toString());
+
+
+    }
+
+    public static void dfs(int Node, boolean[] visit, int cnt) {
+        sb.append(Node + " ");
+        visit[Node] = true;
+        for (int e : edge[Node]) {
+            if (!visit[e]) {
+                dfs(e, visit, cnt + 1);
+            }
+        }
+    }
+
+    public static void bfs(int Node, boolean[] visit, int cnt) {
+        sb.append(Node + " ");
+
+        for (int e : edge[Node]) {
+            if (!visit[e]) {
+                visit[e] = true;
+                queue.add(e);
+            }
+        }
+    }
+}
