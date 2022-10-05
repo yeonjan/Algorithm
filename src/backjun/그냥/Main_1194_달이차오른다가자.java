51a5908683a8d5e23ea2dcdd7a48b7d32a284424
Yeonjin Jeong <y_jin99@naver.com>
2022-10-05 22:25:48 +0900

    add .gitignore

--- src/backjun/그냥/Main_1194_달이차오른다가자.java
+++ src/backjun/그냥/Main_1194_달이차오른다가자.java
@@ -1,2 +1,100 @@
-package backjun.그냥;public class Main_1194_달이차오른다가자 {
+package backjun.그냥;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main_1194_달이차오른다가자 {
+    static int n, m, ans = Integer.MAX_VALUE;
+    static boolean[][][] visited;
+    static char[][] map;
+    static int[][] delta = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    static class Minsick {
+        int r, c, key, dist;
+
+        public Minsick(int r, int c, int key, int dist) {
+            this.r = r;
+            this.c = c;
+            this.key = key;
+            this.dist = dist;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new char[n][m];
+        visited = new boolean[n][m][65];
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == '0') {
+                    bfs(i, j);
+                }
+            }
+        }
+        if (ans == Integer.MAX_VALUE) ans = -1;
+        System.out.println(ans);
+
+    }
+
+    public static void bfs(int i, int j) {
+        //A-Z = 65-90, a-z = 97-122
+        LinkedList<Minsick> queue = new LinkedList<>();
+        queue.offer(new Minsick(i, j, 0, 0));
+        visited[i][j][0] = true;
+
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                Minsick now = queue.poll();
+                if (map[now.r][now.c] == '1') {
+                    ans = Math.min(now.dist, ans);
+                }
+                int dist = now.dist;
+
+                for (int d = 0; d < 4; d++) {
+                    int dr = now.r + delta[d][0];
+                    int dc = now.c + delta[d][1];
+                    int key = now.key;
+
+
+                    if (dr < 0 || dc < 0 || dr >= n || dc >= m) continue;
+                    if (!visited[dr][dc][key]) {
+                        visited[dr][dc][key] = true;
+                        int next = map[dr][dc];
+                        if (next == '#') continue;
+                        else if (next == '.' || next == '1' || next == '0')
+                            queue.offer(new Minsick(dr, dc, key, dist + 1));
+                        else if (map[dr][dc] >= 97) {
+                            int num = map[dr][dc] - 'a';
+                            //새로운 키면 업데이트
+                            if ((key & (1 << num)) == 0) {
+                                key = key | (1 << num);
+                            }
+                            queue.offer(new Minsick(dr, dc, key, dist + 1));
+                            visited[dr][dc][key] = true;
+                        } else {
+                            int num = map[dr][dc] - 'A';
+                            //키 가짐
+                            if ((key & (1 << num)) != 0) {
+                                queue.offer(new Minsick(dr, dc, key, dist + 1));
+                                visited[dr][dc][key] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/backjun/그냥/Main_2636_치즈_DFS.java
+++ src/backjun/그냥/Main_2636_치즈_DFS.java
@@ -1,2 +1,86 @@
-package backjun.그냥;public class Main_2636_치즈 {
+package backjun.그냥;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2636_치즈_DFS {
+
+    static int w,h,cnt;
+    static int[][]map,map2,delta= {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    static boolean[][]visited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        h= Integer.parseInt(st.nextToken());
+        w= Integer.parseInt(st.nextToken());
+        map= new int[h][w];
+        map2= new int[h][w];
+
+        for (int i = 0; i <h; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <w; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int day = 0;
+        int last = 0;
+
+        while (true) {
+            visited= new boolean[h][w];
+            count();
+            if (cnt== 0) break;
+
+            day++;
+            last =cnt;
+            dfs(0, 0);
+
+        }
+
+        System.out.println(day);
+        System.out.println(last);
+
+    }
+
+    public static void dfs(int r, int c) {
+        for (int d = 0; d < 4; d++) {
+            int dr = r +delta[d][0];
+            int dc = c +delta[d][1];
+
+            if (check(dr, dc)) continue;
+
+            if (!visited[dr][dc]) {
+                visited[dr][dc] = true;
+                if (map[dr][dc] == 0) {
+                    dfs(dr, dc);
+                } else {
+                    map[dr][dc] = 0;
+                }
+            }
+        }
+    }
+
+    public static void count() {
+        cnt= 0;
+        for (int i = 0; i <h; i++) {
+            for (int j = 0; j <w; j++) {
+                if (map[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    public static boolean check(int dr, int dc) {
+        if (dr < 0 || dc < 0 || dr >=h|| dc >=w) return true;
+        return false;
+    }
+
+
 }
+
