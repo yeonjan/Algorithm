bf28a5cb312eaeb8b0ad553d99456a5b1fa5d659
Yeonjin Jeong <y_jin99@naver.com>
2022-10-27 21:58:22 +0900

    스터디 준비

--- src/study/공통문제/Main_17615_볼모으기.java
+++ src/study/공통문제/Main_17615_볼모으기.java
@@ -28,40 +28,45 @@ public class Main_17615_볼모으기 {
         n = Integer.parseInt(br.readLine());
 
         char[] input = br.readLine().toCharArray();
-        cntBall.put('R', 0);
-        cntBall.put('B', 0);
-        char pre = input[0];
-        int cnt = 1;
+        char cur, pre = input[0];
+        int cntR = 0, cntB = 0, cnt = 1;
         for (int i = 1; i <= input.length; i++) {
-            char cur;
+
             if (i == input.length) cur = 'A';
             else cur = input[i];
 
+            //이전 볼과 현재볼의 색이 같을 떄
             if (pre == cur) {
                 cnt++;
                 continue;
             }
-
-            Ball ball;
+            //이전 볼과 현재볼의 색이 다를 떄
             if (pre == 'R') {
-                ball = new Ball(cnt, 'R');
-                cntBall.replace('R', cnt + cntBall.get('R'));
+                list.add(new Ball(cnt, 'R'));
+                cntR += cnt;
             } else {
-                ball = new Ball(cnt, 'B');
-                cntBall.replace('B', cnt + cntBall.get('B'));
+                list.add(new Ball(cnt, 'B'));
+                cntB += cnt;
             }
-            list.add(ball);
+
             pre = cur;
             cnt = 1;
-
         }
-        ans = Math.min(cntBall.get('R'), cntBall.get('B'));
 
+        cntBall.put('R', cntR);
+        cntBall.put('B', cntB);
+
+        ans = Math.min(cntR, cntB);
+
+        //첫번째 공과 마지막 공 더미
         Ball first = list.get(0);
         Ball last = list.get(list.size() - 1);
+        //색이 같을 때
         if (first.color == last.color) {
             ans = Math.min(ans, cntBall.get(first.color) - Math.max(first.size, last.size));
-        } else {
+        }
+        //색이 다를 때
+        else {
             int f = cntBall.get(first.color) - first.size;
             int l = cntBall.get(last.color) - last.size;
             ans = Math.min(f, l);
