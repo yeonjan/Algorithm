5f0cb45809108e548a0db3336a7401864c10d94e
Yeonjin Jeong <y_jin99@naver.com>
2022-11-03 21:39:26 +0900

    주석 추가

--- src/study/공통문제/Main_1446_지름길.java
+++ src/study/공통문제/Main_1446_지름길.java
@@ -6,24 +6,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_1446_지름길 {
-    static int[][] adj, dist;
+    static int[][] dist;
     static int n, d;
 
-    static class Node {
-        int v;
-        int cost;
-
-        public Node(int v, int cost) {
-            this.v = v;
-            this.cost = cost;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
+
+        //i에서 j까지 거리를 저장한 배열
         dist = new int[d + 1][d + 1];
         for (int i = 0; i <= d; i++) {
             for (int j = i + 1; j <= d; j++) {
@@ -31,13 +24,16 @@ public class Main_1446_지름길 {
             }
         }
 
+        //지름길 입력
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
+            int shortcut = Integer.parseInt(st.nextToken());
+            //지름길 도착지가 고속도로보다 큰 경우 추가x
             if (end > d) continue;
-            dist[start][end] = Math.min(dist[start][end], value);
+            //지름길이 그냥 간 거리보다 짧다면 배열에 반영
+            dist[start][end] = Math.min(dist[start][end], shortcut);
         }
 
         dijkstra();
@@ -48,20 +44,26 @@ public class Main_1446_지름길 {
 
     public static void dijkstra() {
         boolean[] visited = new boolean[d + 1];
-        int minNode = 0;
+        int selectedNode = 0;
 
-        for (int v = 0; v <= d; v++) {
-            int minValue = Integer.MAX_VALUE;
-            visited[minNode] = true;
-            for (int i = 0; i <= d; i++) {
-                if (visited[i]) continue;
-                if (i > minNode) {
-                    dist[0][i] = Math.min(dist[0][i], dist[0][minNode] + dist[minNode][i]);
+        //노드의 횟수만큼 반복(d)
+        for (int i = 0; i <= d; i++) {
+            int minDist = Integer.MAX_VALUE;
+            visited[selectedNode] = true;
+
+            //다음으로 방문할 노드 찾기
+            for (int to = 1; to <= d; to++) {
+                //이미 방문한 노드는 패스
+                if (visited[to]) continue;
+                //minNode 기준으로 0에서 부터의 거리 갱신
+                if (to > selectedNode) {
+                    dist[0][to] = Math.min(dist[0][to], dist[0][selectedNode] + dist[selectedNode][to]);
                 }
 
-                if (minValue > dist[0][i]) {
-                    minNode = i;
-                    minValue = dist[0][i];
+                //방문하지 않은 노드 중 비용이 가장 적은 노드 찾기
+                if (minDist > dist[0][to]) {
+                    selectedNode = to;
+                    minDist = dist[0][to];
                 }
             }
         }
