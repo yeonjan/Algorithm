9755f039ec210ed3ed3316fc697ee323606deecc
Yeonjin Jeong <y_jin99@naver.com>
2022-11-10 23:19:16 +0900

    익명 객체 -> 람다식

--- src/study/공통문제/Main_13904_과제.java
+++ src/study/공통문제/Main_13904_과제.java
@@ -1,12 +1,11 @@
 package study.공통문제;
 
 import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Arrays;
-        import java.util.Comparator;
-        import java.util.PriorityQueue;
-        import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main_13904_과제 {
     static int N, sum = 0;
@@ -15,19 +14,13 @@ public class Main_13904_과제 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) return Integer.compare(o2[1], o1[1]);
-                else return Integer.compare(o2[0], o1[0]);
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1[0] == o2[0]) return Integer.compare(o2[1], o1[1]);
+            else return Integer.compare(o2[0], o1[0]);
         });
-        PriorityQueue<int[]> able = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) return Integer.compare(o1[0], o2[0]);
-                else return Integer.compare(o2[1], o1[1]);
-            }
+        PriorityQueue<int[]> able = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] == o2[1]) return Integer.compare(o1[0], o2[0]);
+            else return Integer.compare(o2[1], o1[1]);
         });
 
         for (int i = 0; i < N; i++) {
@@ -40,7 +33,7 @@ public class Main_13904_과제 {
         int day = pq.peek()[0] + 1;
 
         while (--day > 0) {
-            while (!pq.isEmpty()&&pq.peek()[0] == day) {
+            while (!pq.isEmpty() && pq.peek()[0] == day) {
                 able.offer(pq.poll());
             }
             if (!able.isEmpty()) sum += able.poll()[1];
