d428e1630f7e4619c1312ab29fddbcabea9b65f6
Yeonjin Jeong <y_jin99@naver.com>
2022-12-29 12:51:04 +0900

    속도 개선 정답을 담을 때 StringBuilder -> 배열로 변경

--- src/study/공통문제/Main_2473_세용액.java
+++ src/study/공통문제/Main_2473_세용액.java
@@ -7,41 +7,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_2473_세용액 {
-    static long ans = Long.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        long[] sol = new long[n];
+
+        int n = Integer.parseInt(br.readLine()); // 용액의 수
+        long min = Long.MAX_VALUE; // 0에 가장 가까운 특성값
+        long[] ans = new long[3]; // 정답인 세용액
+        long[] sol = new long[n]; // 입력으로 들어오는 모든 용액
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             sol[i] = Integer.parseInt(st.nextToken());
         }
 
+        //순차적으로 탐색하기 위해 정렬
         Arrays.sort(sol);
 
-        END:
+        //세용액 i,l,r
+        FIND_ZERO:
         for (int i = 0; i < n; i++) {
             int l = i + 1;
             int r = n - 1;
 
             while (l < r) {
                 long sum = sol[i] + sol[l] + sol[r];
-                if (ans > Math.abs(sum)) {
-                    ans = Math.abs(sum);
-                    sb.setLength(0);
-                    sb.append(sol[i]).append(" ").append(sol[l]).append(" ").append(sol[r]);
+
+                //세용액을 더한값의 절댓값이 최소라면
+                if (min > Math.abs(sum)) {
+                    min = Math.abs(sum);
+                    ans[0] = sol[i];
+                    ans[1] = sol[l];
+                    ans[2] = sol[r];
                 }
 
                 if (sum < 0) l++;
                 else if (sum > 0) r--;
-                else {
-                    sb.append(sol[i]).append(" ").append(sol[l]).append(" ").append(sol[r]);
-                    break END;
-                }
+                else break FIND_ZERO;
             }
         }
-        System.out.println(sb);
+
+        System.out.println(ans[0] + " " + ans[1] + " " + ans[2]);
     }
 }
