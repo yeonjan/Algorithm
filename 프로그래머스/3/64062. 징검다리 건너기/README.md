0fda86d376da2b2fff156b7df0a869bbfd5168d2
Yeonjin Jeong <60567697+yeonjan@users.noreply.github.com>
2025-05-12 16:26:33 +0900

    [level 3] Title: 징검다리 건너기, Time: 75.58 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062#) 
 
 ### 성능 요약
 
-메모리: 86.6 MB, 시간: 58.07 ms
+메모리: 84.2 MB, 시간: 75.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 12일 15:35:42
+2025년 05월 12일 16:26:32
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,43 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public class Data{
-        int index;
-        int value;
-        public Data(int i,int v){
-            this.index=i;
-            this.value=v;
-        }
-    }
+
     public int solution(int[] stones, int k) {
         int n=stones.length;
+        int left=0;
+        int right=200000000;
         
-        int ans=Integer.MAX_VALUE;
-        ArrayDeque<Data> deque=new ArrayDeque<>();
-        
-        for(int i=0;i<n;i++){
-            int stone=stones[i];
+        while(left<=right){
+            int mid=(left+right)/2;
             
-            //범위안에 있는것만 남기기
-            while(!deque.isEmpty()&&k<i-deque.peekFirst().index+1){
-                deque.pollFirst();
+            if(canGo(mid,stones,k)){
+                left=mid+1;
+            }else{
+                right=mid-1;
             }
-            
-            //지금 넣을 stone값보다 작은건 버리기(어차피 다음 비교에서도 최대가 아님)
-            while(!deque.isEmpty()&&deque.peekLast().value<stone){
-                deque.pollLast();
-            }
-                
-            deque.offer(new Data(i,stone));
-            if(k<=i+1){
-                //k가 완성된 경우 정답과 비교
-                ans=Math.min(ans,deque.peekFirst().value);
-            
-            }
-
         }
         
+           
+        return right;
+    }
+    public boolean canGo(int value,int[] stones,int k){
+        int cnt=0;
+        for(int stone:stones){
+            if(stone<value){
+                cnt++;
+            }else{
+                cnt=0;
+            }
+            if(cnt==k){
+                return false;
+            }
+        }
+        return true;
         
-        return ans;
     }
 }
