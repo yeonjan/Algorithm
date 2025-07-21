f597d20fe75bc1dce2ed9b3c0eafca41e5f54a31
Yeonjin Jeong <60567697+yeonjan@users.noreply.github.com>
2025-07-21 11:22:29 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.12 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java) 
 
 ### 성능 요약
 
-메모리: 90.2 MB, 시간: 1.18 ms
+메모리: 99.4 MB, 시간: 1.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 17:38:37
+2025년 07월 21일 11:22:27
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,25 +1,21 @@
 import java.util.*;
 class Solution {
-    static int server=1;
-    static int count=0;
     public int solution(int[] players, int m, int k) {
-        PriorityQueue<int[]> pq=new PriorityQueue<>((o1,o2)->Integer.compare(o1[1],o2[1]));//증설된 서버수,만료시간
-        //players 흐르면서 pq에 증설한 서버 넣기
-        
-        for(int i=0;i<24;i++){
-            int player=players[i];
+        int count=0;
+        int server=0;
+        //증설서버, 소멸시간
+        PriorityQueue<int[]> pq=new PriorityQueue<>((o1,o2)->Integer.compare(o1[1],o2[1]));
+        for(int t=0;t<24;t++){
             
-            //만료 서버 처리
-            while(!pq.isEmpty()&&pq.peek()[1]<=i){
-                int[] s=pq.poll();
-                server-=s[0];
+            while(!pq.isEmpty()&&pq.peek()[1]<=t){
+                server-=pq.poll()[0];
             }
             
-            //증설필요
-            int neededServer=player/m+1;
-            if(neededServer>server){
-                pq.offer(new int[]{neededServer-server,i+k});
+//             서버 증설
+            int neededServer=players[t]/m;
+            if(server<neededServer){
                 count+=neededServer-server;
+                pq.offer(new int[]{neededServer-server,t+k});
                 server=neededServer;
             }
             
