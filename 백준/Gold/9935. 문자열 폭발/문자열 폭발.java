a45a720ebd73764e10632d1b4a90edb4ae57b452
Yeonjin Jeong <60567697+yeonjan@users.noreply.github.com>
2025-08-25 12:01:28 +0900

    [Gold IV] Title: 문자열 폭발, Time: 488 ms, Memory: 62068 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -8,35 +8,16 @@ public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String[] str = br.readLine().split("");
-        String[] bomb = br.readLine().split("");
-        int size = bomb.length;
-
-        Stack<String> stack = new Stack<>();
-        for (String s : str) {
-            stack.push(s);
-            if (stack.peek().equals(bomb[size - 1])) {
-                boolean isBomb = true;
-                int cnt = 0;
-                if (stack.size() < size) continue;
-                for (int i = size - 1; i >= 0; i--) {
-                    if (!stack.peek().equals(bomb[i])) {
-                        isBomb = false;
-                        break;
-                    }
-                    cnt++;
-                    stack.pop();
-                }
-                if (!isBomb) {
-                    for (int i = cnt; i > 0; i--) {
-                        stack.push(bomb[size - i]);
-                    }
-                }
-            }
+        String str = br.readLine();
+        String bomb = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+            bomb(bomb, stack);
         }
-        if (stack.isEmpty()){
+        if (stack.isEmpty()) {
             sb.append("FRULA");
-        }else {
+        } else {
             while (!stack.isEmpty()) {
                 sb.append(stack.pop());
             }
@@ -47,5 +28,29 @@ public class Main {
 
     }
 
+    public static void bomb(String bomb, Stack<Character> stack) {
+        int size = bomb.length();
+        if (stack.size() < size) return;
+        if (!stack.peek().equals(bomb.charAt(size - 1))) return;
+
+        boolean isBomb = true;
+        int cnt = 0;
+        for (int i = 0; i < size; i++) {
+            if (!stack.peek().equals(bomb.charAt(size - i - 1))) {
+                isBomb = false;
+                break;
+            }
+            stack.pop();
+            cnt++;
+
+        }
+        if (!isBomb) {
+            for (int i = cnt; i > 0; i--) {
+                stack.push(bomb.charAt(size - i));
+            }
+        }
+    }
+
 
 }
+
