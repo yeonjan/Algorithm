9284fad3592b60a96ed7d5e41db3526daab89980
Yeonjin Jeong <60567697+yeonjan@users.noreply.github.com>
2025-09-28 22:27:27 +0900

    [Gold III] Title: 불!, Time: 528 ms, Memory: 47756 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -3,99 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 public class Main {
-    static int R, C;
-    static String[][] map;
     static int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int R, C;
+    static char[][] map;
+    static ArrayDeque<int[]> fireQ = new ArrayDeque<>();
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new String[R][C];
-        int[] start = new int[2];
-        Queue<int[]> fireQ = new ArrayDeque<>();
-        for (int i = 0; i < R; i++) {
-            map[i] = br.readLine().split("");
-            for (int j = 0; j < C; j++) {
-                if (map[i][j].equals("J")) {
-                    start = new int[]{i, j};
-                    map[i][j] = ".";
-                }
-                if (map[i][j].equals("F")) {
-                    fireQ.add(new int[]{i, j});
-                }
+        int[] jihoon = new int[2];
+        map = new char[R][C];
 
+
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == 'J') {
+                    jihoon = new int[]{i, j};
+                    map[i][j] = '.';
+                } else if (map[i][j] == 'F') fireQ.offer(new int[]{i, j});
             }
         }
-        bfs(start, fireQ);
-        System.out.println();
+        int solve = solve(jihoon);
 
 
+        System.out.println(solve < 0 ? "IMPOSSIBLE" : solve);
     }
 
-    public static void bfs(int[] start, Queue<int[]> fireList) {
-        Queue<int[]> queue = new ArrayDeque<>(); // r,c,time
+    public static int solve(int[] jihoon) {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+
         boolean[][] visited = new boolean[R][C];
-        int escapeTime = -1;
-        int time = 0;
-        queue.add(new int[]{start[0], start[1]});
-        visited[start[0]][start[1]] = true;
+        visited[jihoon[0]][jihoon[1]] = true;
+        queue.offer(jihoon);
 
-        Escape:
+        int time = 1;
         while (!queue.isEmpty()) {
-            time++;
-            fire(fireList);
-
             int size = queue.size();
-
+            fire();
+            time++;
             for (int i = 0; i < size; i++) {
-                int[] jihun = queue.poll();
-
+                int[] pos = queue.poll();
                 for (int d = 0; d < 4; d++) {
-                    int dr = jihun[0] + delta[d][0];
-                    int dc = jihun[1] + delta[d][1];
+                    int dr = pos[0] + delta[d][0];
+                    int dc = pos[1] + delta[d][1];
+                    if (dr < 0 || dc < 0 || dr >= R || dc >= C) return time - 1;
 
-                    if (dr < 0 || dc < 0 || dr >= R || dc >= C) {
-                        escapeTime = time;
-                        break Escape;
-                    }
-                    if (map[dr][dc].equals(".") && !visited[dr][dc]) {
-                        queue.add(new int[]{dr, dc});
+                    if (map[dr][dc] == '.' && !visited[dr][dc]) {
+                        queue.offer(new int[]{dr, dc});
                         visited[dr][dc] = true;
                     }
                 }
-
             }
-
         }
-
-        System.out.println(escapeTime != -1 ? escapeTime : "IMPOSSIBLE");
-
+        return -1;
 
     }
 
-    public static void fire(Queue<int[]> fireQueue) {
-        int size = fireQueue.size();
+    public static void fire() {
+        int size = fireQ.size();
         for (int i = 0; i < size; i++) {
-            int[] fire = fireQueue.poll();
+            int[] pos = fireQ.poll();
             for (int d = 0; d < 4; d++) {
-                int dr = fire[0] + delta[d][0];
-                int dc = fire[1] + delta[d][1];
+                int dr = pos[0] + delta[d][0];
+                int dc = pos[1] + delta[d][1];
                 if (dr < 0 || dc < 0 || dr >= R || dc >= C) continue;
-                if (map[dr][dc].equals(".")) {
-                    map[dr][dc] = "F";
-                    fireQueue.add(new int[]{dr, dc});
-                }
+                if (map[dr][dc] == '#'||map[dr][dc]=='F') continue;
+                map[dr][dc] = 'F';
+                fireQ.offer(new int[]{dr, dc});
             }
-
-
         }
-
-
     }
 
 
